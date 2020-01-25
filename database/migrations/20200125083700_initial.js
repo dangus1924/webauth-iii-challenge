df9a025a823b7de2ebdb4277f8c083e4e7ddef8f
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-25 08:54:46 -0500

    created tables with knex

---
+++ database/migrations/20200125083700_initial.js
@@ -0,0 +1,15 @@
+
+exports.up = async function(knex) {
+  await knex.schema.createTable('users', (table) => {
+      table.increments()
+      table.string('username', 128)
+        .notNullable()
+        .unique()
+      table.string('password', 128).notNullable()
+      table.string('deparment', 128).notNullable()
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('users')
+};

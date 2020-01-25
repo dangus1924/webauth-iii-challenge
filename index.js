e30006275206fa1be505414951a8e97ff80ad47a
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-25 10:07:41 -0500

    imported routes into index file

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 
+const authRoute = require('./routes/auth/auth-routes')
+const userRoute = require('./routes/users/users-route')
+
 const port = 5000;
 const server = express()
 
 server.use(helmet())
 server.use(express.json())
 server.use(cors())
+
+server.use('/api/auth', authRoute)
+server.use('/api/user', userRoute)
 
 server.get('/', (req, res, next) => {
     res.json({

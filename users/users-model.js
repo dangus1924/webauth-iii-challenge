5582cdb6dd79780abbe41cf9ee5a307bbdf20be8
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2020-01-25 09:21:32 -0500

    created basic model

---
+++ users/users-model.js
@@ -0,0 +1,20 @@
+const db = require('../database/db-config')
+
+async function add(user) {
+    const [id] = await db('users').insert(user)
+    return findById({ id })
+}
+
+function find() {
+    return db('users').select('username', 'password', 'department');
+}
+
+function findById(id) {
+    return db('user').where(id)
+}
+
+module.exports = {
+    add,
+    find,
+    findById,
+}
